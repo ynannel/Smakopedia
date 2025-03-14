641f5aa3305fba670b63dc3542789ee738cef8e4
Yana Pasternak <yana.pasternak@gmail.com>
2025-03-14 14:27:27 +0100

    Width 900 na width 1000

---
+++ Smakopedia/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Smakopedia;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.LeftButton == MouseButtonState.Pressed)
+        {
+            DragMove();
+        }
+    }
+
+    private void OpenRecipeSelection_Click(object sender, RoutedEventArgs e)
+    {
+        RecipeSelectionWindow recipeWindow = new RecipeSelectionWindow();
+        recipeWindow.Show();
+    }
+
+    private void CreateRecipeWindow_Click(object sender, RoutedEventArgs e)
+    {
+        CreateRecipeWindow recipeWindow = new CreateRecipeWindow();
+        recipeWindow.Show();
+    }
+
+}
